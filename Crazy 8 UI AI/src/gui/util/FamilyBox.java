ce37b95d13a4be665d592ed745209da7dd5f54ad
Henchel-Santillan <henchelsantillan@gmail.com>
2020-07-29 20:09:56 -0400

    FamilyBox completed

--- Crazy 8 UI AI/src/gui/util/FamilyBox.java
+++ Crazy 8 UI AI/src/gui/util/FamilyBox.java
@@ -1,17 +1,20 @@
 package gui.util;
 
+import javafx.application.Platform;
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.ReadOnlyStringWrapper;
 
 import javafx.geometry.Pos;
 
-import javafx.scene.Node;
-
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.Priority;
 
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 
 public class FamilyBox {
@@ -20,38 +23,51 @@ public class FamilyBox {
 	private final ComboBox<String> box;
 	private final ReadOnlyStringWrapper selected;
 	
+	private final ObservableList<String> fonts;
+	private final FilteredList<String> filteredFonts;
+	
 	//TODO: Search Function + display in box
 	public FamilyBox() {
 		selected = new ReadOnlyStringWrapper();
 		
+		fonts = FXCollections.observableArrayList();
+		fonts.addAll(Font.getFamilies());
+		
+		filteredFonts = new FilteredList<>(fonts, p -> true);
+		
 		box = new ComboBox<>();
 		box.setEditable(true);
-		box.getItems().addAll(Font.getFamilies());
 		
 		box.selectionModelProperty().addListener((observable, oldValue, newValue) -> {
 			selected.set(newValue.getSelectedItem());
+			box.getEditor().setText(newValue.getSelectedItem());
 		});
 		
-		box.getEditor().accessibleTextProperty().addListener((observable, oldValue, newValue) -> {
+		box.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
 			if (!newValue.matches("/^[A-Za-z]+$/")) {
 				box.getEditor().setText(newValue.replaceAll("[^a-zA-Z]", ""));
 			}
+			
+			final TextField editor = box.getEditor();
+			final String selectedItem = box.getSelectionModel().getSelectedItem();
+			
+			Platform.runLater(new Runnable() {
+				@Override
+				public void run() {
+					if (selectedItem == null || !selectedItem.contentEquals(editor.getText())) {
+						filteredFonts.setPredicate(item -> {
+							if (item.toLowerCase().startsWith(newValue.toLowerCase())) {
+								return true;
+							}
+							return false;
+						});
+					}
+				}
+			});
 		});
 		
-		box.getEditor().focusedProperty().addListener((observable, oldValue, newValue) -> {
-			//TODO: Use iterative binary or jump search to determine location of Font family
-			String input = box.getEditor().getText().trim();
-			
-			//Hide all none-matching
-			//TODO: Use sorted and filteredlist
-			if (!newValue && input != null && !input.isEmpty()) {
-				int index = binarySearch(box.getItems(), input);
-				
-				if (index != -1) {
-					box.getEditor().setText(box.getItems().get(index));
-				}
-			}
-		});
+		box.setItems(filteredFonts);
+		box.getEditor().setAlignment(Pos.CENTER);
 		
 		model = new StackPane(box);
 	}
@@ -76,25 +92,5 @@ public class FamilyBox {
 			return Font.font(selected.get());
 		
 		throw new IllegalStateException("No font selected.");
-	}
-	
-	private final int binarySearch(ObservableList<String> array, String input) {
-		int left = 0, right = array.length - 1;
-		
-		while (left <= right) {
-			int median = left + (right - left) / 2;
-			
-			if (array[median].equalsIgnoreCase(input)) {
-				return median;
-			}
-			
-			if (array[median].compareToIgnoreCase(input) < 0) {
-				left = median + 1;
-			} else {
-				right = median - 1;
-			}
-		}
-		
-		return -1;
 	}
 }
