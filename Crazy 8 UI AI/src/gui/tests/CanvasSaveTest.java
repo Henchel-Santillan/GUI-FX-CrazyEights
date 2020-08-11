5305317c9540baa8a6caa202c43d6b7599375fd7
Henchel-Santillan <henchelsantillan@gmail.com>
2020-08-10 21:10:56 -0400

    Canvas Save Test: 'save as' operation completed

--- Crazy 8 UI AI/src/gui/tests/CanvasSaveTest.java
+++ Crazy 8 UI AI/src/gui/tests/CanvasSaveTest.java
@@ -2,9 +2,17 @@ package gui.tests;
 
 import gui.util.ToggleSwitch;
 
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+import java.util.logging.Level;
+
 import java.io.File;
+import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.stage.Stage;
 import javafx.stage.Screen;
 import javafx.stage.FileChooser;
@@ -13,6 +21,7 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.geometry.Rectangle2D;
 
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
@@ -31,11 +40,19 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Label;
 
+import javafx.embed.swing.SwingFXUtils;
+import javax.imageio.ImageIO;
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import java.awt.image.RenderedImage;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
 import javafx.scene.paint.Color;
 
 public class CanvasSaveTest extends Application {
+	
+	public String projectname = "Untitled";
 	
 	Rectangle2D screen;
 	Scene scene;
@@ -68,12 +85,24 @@ public class CanvasSaveTest extends Application {
 		
 		root.setCenter(stack);
 		
+		label = new Label("Context save: ");
+		toggle = new ToggleSwitch(80.0d);
+		
 		save = new MenuItem("Save");
 		save.setOnAction(e -> {
 			
-			//TODO: Check if file in file system
-			//if none, create a new one
-			//Prompt save
+			//TODO: get project name, probably a path
+			File file = new File(projectname);
+			
+			if (file.exists() && !file.isDirectory()) {
+				//TODO: Save operation on file as found in saveas
+			} else {
+				FileChooser chooser = new FileChooser();
+				chooser.setTitle("Save");
+				chooser.showSaveDialog(stage);
+				
+				//Save operation on file as found in saveas with added check
+			}
 			
 			e.consume();
 		});
@@ -81,13 +110,39 @@ public class CanvasSaveTest extends Application {
 		saveas = new MenuItem("Save As");
 		saveas.setOnAction(e -> {
 			FileChooser chooser = new FileChooser();
+			chooser.setTitle("Save As");
+		
+			chooser.setInitialDirectory(new File(System.getProperty("user.home") + "/Downloads/"));
+			chooser.setInitialFileName("project.png");
 			
+			//TODO: check if chosen is a directory
 			File chosen = chooser.showSaveDialog(stage);
 			
 			if (chosen != null) {
+				try {
+					SnapshotParameters snaps = new SnapshotParameters();
+					snaps.setFill(Color.TRANSPARENT);
+					
+					BufferedImage image = SwingFXUtils.fromFXImage(canvas.snapshot(snaps, null), null);
+					BufferedImage imagergb = new BufferedImage(image.getWidth(), image.getHeight(), 
+							BufferedImage.OPAQUE);
+					Graphics2D graphics = imagergb.createGraphics();
+					graphics.drawImage(image, 0, 0, null);
+					
+					ImageIO.write(imagergb, "png", chosen);
+					/*WritableImage writable = canvas.snapshot(snaps, new WritableImage(
+							(int) canvas.getWidth(), (int) canvas.getHeight()));
+					RenderedImage image = SwingFXUtils.fromFXImage(writable, null);
+					ImageIO.write(image, "png", chosen);*/
+					
+				} catch (IOException ex) {
+					Logger.getLogger(CanvasSaveTest.class.getName()).log(Level.SEVERE, null, ex);
+				}
 				
+				if (toggle.isSelected()) {
+					context.save();
+				}
 			}
-			//TODO: showSaveDialog()
 			
 			e.consume();
 		});
@@ -101,7 +156,6 @@ public class CanvasSaveTest extends Application {
 			File chosen = chooser.showOpenDialog(stage);
 			
 			if (chosen != null) {
-				//TODO: Image resizing?
 				context.drawImage(new Image(chosen.toURI().toString()), 0, 0);
 			}
 			
@@ -112,9 +166,6 @@ public class CanvasSaveTest extends Application {
 		file.getItems().addAll(save, saveas, importImage);
 		
 		bar = new MenuBar(file);
-		
-		label = new Label("Context save: ");
-		toggle = new ToggleSwitch(80.0d);
 		
 		ribbon = new HBox(bar, label, toggle.getModel());
 		root.setTop(ribbon);
